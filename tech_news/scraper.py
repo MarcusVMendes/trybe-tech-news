c7c3378a7506ddacae7ddb183a04bc68bc56fb90
MarcusVMendes <marcus.mendes89@gmail.com>
2022-04-13 08:28:43 -0300

    Requisito 4 concluido

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -37,7 +37,35 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css('link[rel="canonical"]::attr(href)').get()
+    title = selector.css(".tec--article__header__title::text").get()
+    ts = selector.css("time::attr(datetime)").get()
+    writer = selector.css(".z--font-bold *::text").get()
+    shares_count = selector.css(".tec--toolbar__item::text").get()
+    comments_count = selector.css("#js-comments-btn")
+    summary = "".join(selector.css(
+        ".tec--article__body > p:nth-child(1) *::text").getall())
+    sources = selector.css(
+        '.tec--badge[rel="noopener nofollow"]::text').getall()
+    categories = selector.css("#js-categories a::text").getall()
+    if type(shares_count) == str:
+        shares_count = int(shares_count[1])
+    if comments_count:
+        comments_count = int(comments_count.attrib["data-count"])
+    if type(categories) == list:
+        categories = [item.strip() for item in categories]
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": ts,
+        "writer": writer.strip() if type(writer) is str else None,
+        "shares_count": shares_count if shares_count is not None else 0,
+        "comments_count": comments_count if type(comments_count) is int else 0,
+        "summary": summary,
+        "sources": [item.strip() for item in sources],
+        "categories": categories
+    }
 
 
 # Requisito 5
